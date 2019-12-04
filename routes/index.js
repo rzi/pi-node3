6164e3b63f0ebb893ae16b42dd24eb9319c1a3b3
rzi <rzi@vp.pl>
2019-12-04 21:51:01 +0100

    form test

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/', function (req, res) {
+  console.log(req.body.title);
+  console.log(req.body.description);
+  //res.send('Post page');
+  res.render('index', { title: 'Express' }, { title2:' express2'});
+});
 
 module.exports = router;
