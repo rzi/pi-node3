8ea9c4d1584410742f310ef43ff9ce7e8f6dfebb
rzi <rzi@vp.pl>
2019-12-05 21:11:15 +0100

    form

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+var html='<p>paragraf</p>';
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.post('/', function (req, res) {
+router.post('/', function (req, res, next) {
   console.log(req.body.title);
   console.log(req.body.description);
   //res.send('Post page');
-  res.render('index', { title: 'Express' }, { title2:' express2'});
+  res.render('index', { title: 'Express',title2: req.body.description , title3:req.body.title});
 });
 
+// // if a callback is specified, the rendered HTML string has to be sent explicitly
+// res.render('index', function (err, html) {
+//   res.send(html)
+// })
+
+// // pass a local variable to the view
+// res.render('user', { name: 'Tobi' }, function (err, html) {
+//   // ...
+// })
 module.exports = router;
